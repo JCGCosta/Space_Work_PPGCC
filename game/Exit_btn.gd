8185622bcaf4628006956314d54ff68ea8324782
Rebeca Torrecilhas <rebeca.torrecilhas@gmail.com>
2022-11-07 06:42:40 -0300

    Tela inicial (Menu)

--- game/Exit_btn.gd
+++ game/Exit_btn.gd
@@ -1,20 +1,20 @@
 extends Button
 
-func _on_Start_mouse_entered() : 
+func _on_Exit_mouse_entered():
 	$Sprite.modulate.r = 0.8
 	$Sprite.modulate.g = 0.8
-	$Sprite.modulate.b = 0.8
-	pass
-	
-func _on_Start_mouse_exited() : 
-	$Sprite.modulate.r = 1
-	$Sprite.modulate.g = 1
-	$Sprite.modulate.b = 1
+	$Sprite.modulate.b = 0.8	
 	pass
 
-func _on_Start_pressed():
+func _on_Exit_mouse_exited():
+	$Sprite.modulate.r = 1
+	$Sprite.modulate.g = 1
+	$Sprite.modulate.b = 1	
+	pass
+
+func _on_Exit_pressed():
 	$Sprite.modulate.r = 0.6
 	$Sprite.modulate.g = 0.6
 	$Sprite.modulate.b = 0.6
-	get_tree().change_scene("res://Main.tscn")
-	pass 
+	get_tree().quit()
+	pass
