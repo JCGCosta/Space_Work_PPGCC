0745090320817ffc8fd3127030771fbf66c0cbc2
Andre Lacomski <andrelacomski1@gmail.com>
2022-11-10 22:38:57 -0300

    movimento entre as orbitas

--- game/Ship.gd
+++ game/Ship.gd
@@ -2,7 +2,9 @@ extends KinematicBody2D
 
 var angle := 90.0
 var speed := 2.0
-var radius_orbit_1 := 250.0
+var orbit_active = 1
+var radius_orbit_1 := 200.0
+var radius_orbit_2 : = 300.0
 var planet := Vector2(505,306)
 
 var controll = {
@@ -15,27 +17,31 @@ var controll = {
 func _physics_process(delta : float) -> void :
 	controll_loop()
 	movement_loop(delta)
-	
+
 func controll_loop():
 	self.controll['up'] = Input.is_action_pressed("ui_up")
 	self.controll['down'] = Input.is_action_pressed("ui_down")
 	self.controll['left'] = Input.is_action_pressed("ui_left")
 	self.controll['right'] = Input.is_action_pressed("ui_right")
-	
-func movement_loop(delta):
-	if(self.controll['left']):
-		angle += delta
-	
-	if(self.controll['right']):
-		angle -= delta
-		
-	if(self.controll['left'] || self.controll['right']):
-		self.position = Vector2 (
-			sin(angle * speed) * radius_orbit_1,
-			cos(angle * speed) * radius_orbit_1 
-		) + planet
-		
 
-	
-	
-	
+func movement_loop(delta):
+	if(self.controll['up'] && self.orbit_active == 1):
+		self.orbit_active = 2
+
+	if(self.controll['down'] && self.orbit_active == 2):
+		self.orbit_active = 1
+
+	if(self.controll['left']):
+		self.angle += delta
+
+	if(self.controll['right']):
+		self.angle -= delta
+
+	var value_orbit = self.radius_orbit_1
+
+	if(self.orbit_active == 2):
+		value_orbit = self.radius_orbit_2
+
+	self.position = Vector2(sin(self.angle * speed) * value_orbit, cos(self.angle * speed) * value_orbit) + planet
+
+	$Sprite.rotation = self.planet.angle_to(self.position) 
